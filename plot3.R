d8d96517c896eaf5082ccc6218033908f4c107e7
sarin.du <rs715714@gmail.com>
2014-05-11 05:39:30 +0700

    fixing some error in legend box of plot 3 and 4

--- plot3.R
+++ plot3.R
@@ -11,7 +11,7 @@ png(filename = 'plot3.png', width = 480, height = 480, unit = 'px')
 plot(t, d$Sub_metering_1, type = 'l', ylab = "Energy sub metering", col = 'black')
 points(t, d$Sub_metering_2, type = 'l', col = "red")
 points(t, d$Sub_metering_3, type='l', col = "blue")
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_1"),
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),
        col = c("black", "red", "blue"), lwd = 1)
 dev.off()
 
--- plot4.R
+++ plot4.R
@@ -12,20 +12,20 @@ par(mfrow=c(2,2))
 
 # 1
 plot(t , as.numeric(d$Global_active_power), 
-     type = 'l', xlab = "", ylab = "Global Active Power (kilowatts)")
+     type = 'l', xlab = "", ylab = "Global Active Power")
 
 # 2
 plot(t, d$Voltage, type = 'l', ylab = "Voltage", xlab = "datetime")
 
 # 3 
 
-plot(t, d$Sub_metering_1, type = 'l', ylab = "Energy sub metering", col = 'black')
+plot(t, d$Sub_metering_1, type = 'l', ylab = "Energy sub metering", xlab = "", col = 'black')
 points(t, d$Sub_metering_2, type = 'l', col = "red")
 points(t, d$Sub_metering_3, type='l', col = "blue")
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_1"),
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),
        col = c("black", "red", "blue"), lwd = 1, bg = "n", bty = "n")
 
 # 4
-plot(t, d$Global_reactive_power, type = 'l', ylab = "Voltage", xlab = "datetime")
+plot(t, d$Global_reactive_power, type = 'l', ylab = "Global_reactive_power", xlab = "datetime")
 
 dev.off()
